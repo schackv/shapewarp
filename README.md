19a45c29cc32eadad7599e077f0be2cdc858f74f
schackv <schackv@users.noreply.github.com>
2014-06-28 15:18:54 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-shapewarp
-=========
-
+# shapewarp
 Contains tools for generalized Procrustes analysis, active shape models and shape-based image warping.
 
-schackv
+
+## Licensing 
+This software is released under the MIT License (see license.md).
+However, please link to or cite https://github.com/schackv/shapewarp if possible.
+
--- shapewarp_demo/demo.py
+++ shapewarp_demo/demo.py
@@ -10,6 +10,8 @@ import shapewarp.ASM as ASM
 import shapewarp.warp as warp
 import shapewarp.plotting as shplt
 import example_data
+import scipy.misc
+from numpy import isnan
 
 def demo():
     """Demonstrate the shape model and warping functionality"""
@@ -59,7 +61,7 @@ def demo():
     image = data.images[ [i.has_image() for i in data.images].index(True) ]
     im = image.load()
     landmarks = image.landmarks()
-    W = warp.Warper(asm.MeanShape,scale=0.3)        # Warp to 0.3 of the full scale of the mean shape
+    W = warp.Warper(asm.MeanShape,scale=0.5)        # Warp to 0.3 of the full scale of the mean shape
     warped_image = W.warp_image(im,landmarks)
     
     f, ax = plt.subplots(1,3)
@@ -72,6 +74,13 @@ def demo():
         plt.axis('image')
         plt.title(titles[idx])
     plt.show()
+    
+    # Save image
+    mask = W.mask
+    warped_image[isnan(warped_image)]=0
+    mask[isnan(mask)] = 0
+    scipy.misc.toimage(warped_image,cmin=0,cmax=1).save('warpedImage.png')
+    scipy.misc.toimage(mask,cmin=0,cmax=1).save('mask.png')
     ######################################
     
     
